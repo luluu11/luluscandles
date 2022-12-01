ec056f49f698ce7afe9de206dee4bc706bf3ef46
Luciana <rabozzilu@gmail.com>
2022-11-30 21:00:53 -0300

    carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CartWidget = () =>{
     return (
 <button type="button" className="btn btn-primary position-relative">
-    <img src={"images/trash3.svg"} alt={"carrito"} />
+    <img src={"images/cart2.svg"} alt={"carrito"} />
   <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">1</span>
 </button>
         );
