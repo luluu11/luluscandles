01911dd4a7a865ee72b8f79bebe0c72872d2bc33
Luciana <rabozzilu@gmail.com>
2022-12-21 11:25:32 -0300

    reentrega

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Footer from "./components/Footer";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import NavBar from "./components/NavBar";
-import Error404 from "./components/Error404";
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
           <Route path={"/"} element={<ItemListContainer />} />
           <Route path={"/category/:id"} element={<ItemListContainer />} />
           <Route path={"/item/:id"} element={<ItemDetailContainer />} />
-          <Route path={"*"} element={<Error404 />} />
         </Routes>
         <Footer />
       </BrowserRouter>
